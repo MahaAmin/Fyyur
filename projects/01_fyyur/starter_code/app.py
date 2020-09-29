f03c47bcb02fb45403c3b3ffedb21e40439ff1b2
MahaAmin <mahaamin97@gmail.com>
2020-09-29 02:46:22 +0200

    add genres column to venues and actors

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -44,7 +44,7 @@ class Venue(db.Model):
     facebook_link = db.Column(db.String(120))
     website_link = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean, default=False)
-    #genres
+    genres = db.Column(db.String(120))
     shows = db.relationship('Show', backref='show_venue')
 
     def __repr__(self):
@@ -64,7 +64,7 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    #genres = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     website_link = db.Column(db.String(120))
@@ -488,7 +488,7 @@ def shows():
       "start_time": show.start_time
     }
     data.append(tmp)
-    
+
   # data=[{
   #   "venue_id": 1,
   #   "venue_name": "The Musical Hop",
