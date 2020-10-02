2f0d7143c601803e09736b9a41a6b3761d6ef4a0
MahaAmin <mahaamin97@gmail.com>
2020-10-02 18:59:05 +0200

    migration: create genres_venues and genres_artists tables

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -44,7 +44,7 @@ class Venue(db.Model):
     facebook_link = db.Column(db.String(120))
     website_link = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean, default=False)
-    genres = db.Column(db.String(120))
+    genres = db.relationship('Genre_Venue', backref='genre_venue')
     shows = db.relationship('Show', backref='show_venue')
 
     def __repr__(self):
@@ -64,12 +64,12 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     website_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean, default=False)
     shows = db.relationship('Show', backref='show_artist', lazy=True)
+    genres = db.relationship('Genre_Artist', backref='genre_artist')
 
     def __repr__(self):
       return f'''<Artist {self.id}, name: {self.name}, city: {self.city}, 
@@ -93,12 +93,37 @@ class Show(db.Model):
 
 
 class Genre(db.Model):
-  __tablename__ = "generes"
+  __tablename__ = "genres"
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(120))
+  genres_venues = db.relationship('Genre_Venue', backref='genre_venue')
+  genres_artists = db.relationship('Genre_Artist', backref='genre_artist')
+
 
   def __repr__(self):
     return f'''<Genre {self.id}: genre_id: {self.id}, genre_name: {self.name}'''
+
+
+class Genre_Venue(db.Model):
+  __tablename__ = "genres_venues"
+
+  id = db.Column(db.Integer, primary_key=True)
+  genre_id = db.Column(db.Integer, db.ForeignKey('genres.id'), nullable=False)
+  venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), nullable=False)
+
+  def __repr__(self):
+    return f'''<Row_ID {self.id}: genre_id: {self.genre_id}, venue_id: {self.venue_id}'''
+
+
+class Genre_Artist(db.Model):
+  __tablename__ = "genres_artists"
+
+  id = db.Column(db.Integer, primary_key=True)
+  genre_id = db.Column(db.Integer, db.ForeignKey('genres.id'), nullable=False)
+  artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
+
+  def __repr__(self):
+    return f'''<Row_ID {self.id}: genre_id: {self.genre_id}, artist_id: {self.venue_id}'''
 
 
 
